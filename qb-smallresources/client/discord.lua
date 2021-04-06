46e8e82e45b3ae5eb046a99e89aa3b4b4536cf00
GhzGarage <57848836+GhzGarage@users.noreply.github.com>
2021-04-06 18:52:56 -0500

    Updated discord.lua to new version

--- qb-smallresources/client/discord.lua
+++ qb-smallresources/client/discord.lua
@@ -1,23 +1,36 @@
+-- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
+
 Citizen.CreateThread(function()
 	while true do
-        --This is the Application ID (Replace this with you own)
+        -- This is the Application ID (Replace this with you own)
 		SetDiscordAppId()
 
-        --Here you will have to put the image name for the "large" icon.
-		SetDiscordRichPresenceAsset('')
+        -- Here you will have to put the image name for the "large" icon.
+		SetDiscordRichPresenceAsset('logo_name')
         
-        --(11-11-2018) New Natives:
+        -- (11-11-2018) New Natives:
 
-        --Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('')
-        
-        --Here you will have to put the image name for the "small" icon.
-        --SetDiscordRichPresenceAssetSmall('logo-mk3')
+        -- Here you can add hover text for the "large" icon.
+        SetDiscordRichPresenceAssetText('This is a lage icon with text')
+       
+        -- Here you will have to put the image name for the "small" icon.
+        SetDiscordRichPresenceAssetSmall('logo_name')
 
-        --Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('')
+        -- Here you can add hover text for the "small" icon.
+        SetDiscordRichPresenceAssetSmallText('This is a lsmall icon with text')
 
-        --It updates every one minute just in case.
+
+        -- (26-02-2021) New Native:
+
+        --[[ 
+            Here you can add buttons that will display in your Discord Status,
+            First paramater is the button index (0 or 1), second is the title and 
+            last is the url (this has to start with "fivem://connect/" or "https://") 
+        ]]--
+        SetDiscordRichPresenceAction(0, "First Button!", "fivem://connect/localhost:30120")
+        SetDiscordRichPresenceAction(1, "Second Button!", "fivem://connect/localhost:30120")
+
+        -- It updates every minute just in case.
 		Citizen.Wait(60000)
 	end
 end)
