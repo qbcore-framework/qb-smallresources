5e2437b4b54e83c7f34415ac710c3f01eae0627b
LoveFistRP <qbcore@programmer.net>
2021-04-12 14:56:01 -0700

    thread update

--- client/weapdraw.lua
+++ client/weapdraw.lua
@@ -91,18 +91,6 @@ local canFire = true
 local currWeapon = GetHashKey('WEAPON_UNARMED')
 local currentHoldster = nil
 
-QBCore = nil
-
-Citizen.CreateThread(function() 
-    while true do
-        Citizen.Wait(10)
-        if QBCore == nil then
-            TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
-            Citizen.Wait(200)
-        end
-    end
-end)
-
 RegisterNetEvent('weapons:ResetHolster')
 AddEventHandler('weapons:ResetHolster', function()
 	holstered = true
