dd0430d32f40faa6436ada62a4b0c4c8f7722672
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-19 10:18:04 +0300

    Update density.lua

--- client/density.lua
+++ client/density.lua
@@ -1,10 +1,11 @@
 Citizen.CreateThread(function()
 	while true do
 		SetVehicleDensityMultiplierThisFrame(0.05)
-	    SetPedDensityMultiplierThisFrame(1.0)
-	    SetParkedVehicleDensityMultiplierThisFrame(1.0)
+		SetPedDensityMultiplierThisFrame(1.0)
+		SetParkedVehicleDensityMultiplierThisFrame(1.0)
 		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
-
+		Config.SetRandomBoats(EnableBoats)
+		Config.SetRandomTrains(EnableTrains)
 		Citizen.Wait(3)
 	end
 end)
