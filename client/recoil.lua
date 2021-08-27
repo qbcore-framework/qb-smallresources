5e6e04e37a50c2d58cb93827c074dd5695e6cab7
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-27 04:05:19 +0300

    Rewrite hash weapon correctly

--- client/recoil.lua
+++ client/recoil.lua
@@ -1,65 +1,69 @@
 local recoils = {
-	[453432689] = 0.3, -- PISTOL
-	[3219281620] = 0.5, -- PISTOL MK2
-	[1593441988] = 0.2, -- COMBAT PISTOL
-	[584646201] = 0.3, -- AP PISTOL
-	[-1716589765] = 0.6, -- PISTOL .50
-	[324215364] = 0.5, -- MICRO SMG
-	[736523883] = 0.4, -- SMG
-	[2024373456] = 0.1, -- SMG MK2
-	[-270015777] = 0.1, -- ASSAULT SMG
-	[-1074790547] = 0.5, -- ASSAULT RIFLE
-	[961495388] = 0.2, -- ASSAULT RIFLE MK2
-	[-2084633992] = 0.3, -- CARBINE RIFLE
-	[4208062921] = 0.1, -- CARBINE RIFLE MK2
-	[-1357824103] = 0.1, -- ADVANCED RIFLE
+-- resource : https://wiki.gtanet.work/index.php?title=Weapons_Models
+
+-- pistol
+	[453432689] = 1.0, -- Pistol
+	[3219281620] = 1.0, -- PistolMk2
+	[1593441988] = 1.0, -- CombatPistol
+	[-1716589765] = 2.0, -- Pistol50
+	[-1076751822] = 0.5, -- SNSPistol
+	[-771403250] = 0.5, -- HeavyPistol
+	[137902532] = 1.0, -- VintagePistol
+	[-598887786] = 0.9, -- MarksmanPistol
+	[-1045183535] = 1.85, -- Revolver
+	[584646201] = 2.5, -- APPistol
+	[911657153] = 0.1, -- StunGun
+	[1198879012] = 0.9, -- FlareGun
+
+-- SMG
+	[324215364] = 0.2, -- MicroSMG
+	[-619010992] = 1.0, -- MachinePistol
+	[736523883] = 1.2, -- SMG
+	[2024373456] = 0.1, -- SMGMk2
+	[-270015777] = 1.2, -- AssaultSMG
+	[171789620] = 0.2, -- CombatPDW
 	[-1660422300] = 0.1, -- MG
-	[2144741730] = 0.1, -- COMBAT MG
-	[3686625920] = 0.1, -- COMBAT MG MK2
-	[487013001] = 0.4, -- PUMP SHOTGUN
-	[1432025498] = 0.35, -- PUMP SHOTGUN MK2
-	[2017895192] = 0.7, -- SAWNOFF SHOTGUN
-	[-494615257] = 0.4, -- ASSAULT SHOTGUN
-	[-1654528753] = 0.2, -- BULLPUP SHOTGUN
-	[911657153] = 0.1, -- STUN GUN
-	[100416529] = 0.5, -- SNIPER RIFLE
-	[205991906] = 0.7, -- HEAVY SNIPER
-	[177293209] = 0.6, -- HEAVY SNIPER MK2
-	[856002082] = 1.2, -- REMOTE SNIPER
-	[-1568386805] = 1.0, -- GRENADE LAUNCHER
-	[1305664598] = 1.0, -- GRENADE LAUNCHER SMOKE
+	[2144741730] = 0.1, -- CombatMG
+	[3686625920] = 0.1, -- CombatMGMk2
+	[1627465347] = 0.1, -- Gusenberg
+	[-1121678507] = 1.4, -- MiniSMG
+
+-- Rifle
+	[-1074790547] = 0.2, -- AssaultRifle
+	[961495388] = 0.2, -- AssaultRifleMk2
+	[-2084633992] = 0.1, -- CarbineRifle
+	[4208062921] = 0.1, -- CarbineRifleMk2
+	[-1357824103] = 0.1, -- AdvancedRifle
+	[-1063057011] = 0.2, -- SpecialCarbine
+	[2132975508] = 0.2, -- BullpupRifle
+	[1649403952] = 0.3, -- CompactRifle
+
+-- Sniper
+	[100416529] = 0.5, -- SniperRifle
+	[205991906] = 0.7, -- HeavySniper
+	[177293209] = 0.7, -- HeavySniperMk2
+	[-952879014] = 0.35, -- MarksmanRifle	
+
+-- Shotgun
+	[487013001] = 0.4, -- PumpShotgun
+	[2017895192] = 0.7, -- SawnoffShotgun
+	[-1654528753] = 0.2, -- BullpupShotgun
+	[-494615257] = 0.4, -- AssaultShotgun
+	[-1466123874] = 0.7, -- Musket
+	[984333226] = 0.2, -- HeavyShotgun
+	[-275439685] = 0.7, -- DoubleBarrelShotgun
+	[317205821] = 0.2, -- Autoshotgun
+	
+-- other	
+	[-1568386805] = 1.0, -- GrenadeLauncher
 	[-1312131151] = 0.0, -- RPG
-	[1752584910] = 0.0, -- STINGER
-	[1119849093] = 0.01, -- MINIGUN
-	[-1076751822] = 0.2, -- SNS PISTOL
-	[1627465347] = 0.1, -- GUSENBERG
-	[-1063057011] = 0.2, -- SPECIAL CARBINE
-	[-1768145561] = 0.15, -- SPECIAL CARBINE MK2
-	[-771403250] = 0.5, -- HEAVY PISTOL
-	[2132975508] = 0.2, -- BULLPUP RIFLE
-	[-2066285827] = 0.15, -- BULLPUP RIFLE MK2
-	[137902532] = 0.4, -- VINTAGE PISTOL
-	[-1466123874] = 0.7, -- MUSKET
-	[984333226] = 0.2, -- HEAVY SHOTGUN
-	[-952879014] = 0.3, -- MARKSMAN RIFLE
-	[1785463520] = 0.25, -- MARKSMAN RIFLE MK2
-	[1672152130] = 0, -- HOMING LAUNCHER
-	[1198879012] = 0.9, -- FLARE GUN
-	[171789620] = 0.2, -- COMBAT PDW
-	[-598887786] = 0.9, -- MARKSMAN PISTOL
-	[1834241177] = 2.4, -- RAILGUN
-	[-619010992] = 0.3, -- MACHINE PISTOL
-	[-1045183535] = 0.6, -- REVOLVER
-	[-879347409] = 0.6, -- REVOLVER MK2
-	[-275439685] = 0.7, -- DOUBLE BARREL SHOTGUN
-	[1649403952] = 0.3, -- COMPACT RIFLE
-	[317205821] = 0.2, -- AUTO SHOTGUN
-	[125959754] = 0.5, -- COMPACT LAUNCHER
-	[-1121678507] = 0.1, -- MINI SMG		
+	[1119849093] = 0.15, -- Minigun
+	[2138347493] = 0.3, -- Firework
+	[1834241177] = 2.4, -- Railgun
+	[1672152130] = 0.0, -- HomingLauncher
+	[1305664598] = 1.0, -- GrenadeLauncherSmoke
+	[125959754] = 0.5, -- CompactLauncher
 }
-
-
-
 
 Citizen.CreateThread(function()
 	while true do
