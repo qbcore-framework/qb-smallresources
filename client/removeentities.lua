c98b85f61fc4b558f78f09e4c499dcc16bbb7558
Petar Petkov <pgpetkov@protonmail.com>
2021-09-18 09:37:36 +0300

    Remove all parking barriers

--- client/removeentities.lua
+++ client/removeentities.lua
@@ -1,6 +1,18 @@
 local objects = {
     {x = 266.09, y = -349.35, z = 44.74, model = "prop_sec_barier_02b"}, 
     {x = 285.28, y = -355.78, z = 45.13, model = "prop_sec_barier_02a"}, 
+
+    -- Pillbox Hospital Parking Barriers
+    -- Barrier has two props on each side we remove both of them
+    {x = 207.14, y = -803.35, z = 30.98, model = "prop_sec_barrier_ld_01a"},
+    {x = 207.14, y = -803.35, z = 30.98, model = "prop_sec_barier_base_01"},
+    -- Second Barrier
+    {x = 231.08, y = -815.61, z = 30.35, model = "prop_sec_barrier_ld_01a"},
+    {x = 231.08, y = -815.61, z = 30.35, model = "prop_sec_barier_base_01"},
+
+    -- Airport Parking Barriers
+    {x = -793.63, y = -2024.67, z = 8.99, model = "prop_sec_barier_02b"},
+    {x = -798.29, y = -2021.48, z = 9.05, model = "prop_sec_barier_02b"},
 }
 
 Citizen.CreateThread(function()
