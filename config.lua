740af7a0c44b985305ac9e3b1f45d7586230d90c
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-10-21 09:44:34 +0300

    classify items

--- config.lua
+++ config.lua
@@ -8,14 +8,20 @@ Config.JointEffectTime = 60
 Config.RemoveWeaponDrops = true
 Config.RemoveWeaponDropsTimer = 25
 
-Consumeables = {
+ConsumeablesEat = {
     ["sandwich"] = math.random(35, 54),
-    ["water_bottle"] = math.random(35, 54),
     ["tosti"] = math.random(40, 50),
-    ["kurkakola"] = math.random(35, 54),
     ["twerks_candy"] = math.random(35, 54),
     ["snikkel_candy"] = math.random(40, 50),
+}
+
+ConsumeablesDrink = {
+    ["water_bottle"] = math.random(35, 54),
+    ["kurkakola"] = math.random(35, 54),
     ["coffee"] = math.random(40, 50),
+}
+
+ConsumeablesAlcohol = {
     ["whiskey"] = math.random(20, 30),
     ["beer"] = math.random(30, 40),
     ["vodka"] = math.random(20, 40),
