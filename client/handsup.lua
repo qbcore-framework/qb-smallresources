f940d6111bb73b9135cb6c9f30be4cb88577bef6
clementinise <35346472+clementinise@users.noreply.github.com>
2021-10-22 15:23:38 +0200

    Removed handsup on bikes and motorcycles
    You can't "handsup" on bikes and motorcycles since the animation is bugged when on those vehicle. Also, on bikes you can use "X" to do other things like use a melee weapon to hit others, etc. It was hard to use "X" on bikes since it's stopped steering and the animation was bugged :(

--- client/handsup.lua
+++ client/handsup.lua
@@ -10,21 +10,24 @@ RegisterCommand('hu', function()
 	while not HasAnimDictLoaded(animDict) do
 		Citizen.Wait(100)
 	end
-    handsup = not handsup
-    if handsup then
-        TaskPlayAnim(ped, animDict, anim, 8.0, 8.0, -1, 50, 0, false, false, false)
-        if IsPedInAnyVehicle(ped, false) then
-            local vehicle = GetVehiclePedIsIn(ped, false)
-            if GetPedInVehicleSeat(vehicle, -1) == ped then
-                Citizen.CreateThread(function()
-                    while handsup do
-                        Citizen.Wait(1)
-                        DisableControlAction(0, 59, true) -- Disable steering in vehicle
-                    end
-                end)
+    vehicle = GetVehiclePedIsIn(ped)
+    if GetVehicleClass(vehicle) ~= 8 and GetVehicleClass(vehicle) ~= 13 then
+        handsup = not handsup
+        if handsup then
+            TaskPlayAnim(ped, animDict, anim, 8.0, 8.0, -1, 50, 0, false, false, false)
+            if IsPedInAnyVehicle(ped, false) then
+                local vehicle = GetVehiclePedIsIn(ped, false)
+                if GetPedInVehicleSeat(vehicle, -1) == ped then
+                    Citizen.CreateThread(function()
+                        while handsup do
+                            Citizen.Wait(1)
+                            DisableControlAction(0, 59, true) -- Disable steering in vehicle
+                        end
+                    end)
+                end
             end
+        else
+            ClearPedTasks(ped)
         end
-    else
-        ClearPedTasks(ped)
     end
 end, false)
