e50f541bc32f1b8be64c60a1277246544aa9336f
TechnoBehemoth <68982870+TechnoBehemoth@users.noreply.github.com>
2021-10-22 12:37:41 -0400

    Removed Ped Scenario native
    This native removes all scenarios from happening. This includes peds hanging out around restaurants, walking dogs, and all of wildlife spawning. 
    
    Removing the native will bring this all back.

--- client/density.lua
+++ client/density.lua
@@ -3,7 +3,6 @@ Citizen.CreateThread(function()
 		SetVehicleDensityMultiplierThisFrame(0.05)
 	    SetPedDensityMultiplierThisFrame(1.0)
 	    SetParkedVehicleDensityMultiplierThisFrame(1.0)
-		SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
 
 		Citizen.Wait(3)
 	end
