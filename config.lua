2d114f77247ad8a41f55a5e7b6ca23ed5c6e1dc7
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-10-31 21:26:09 +0200

    update config.lua

--- config.lua
+++ config.lua
@@ -98,3 +98,30 @@ Config.Teleports = {
         },
     },
 }
+
+Config.Locations = { -- carwash
+    [1] = {
+        ["label"] = "Hands Free Carwash",
+        ["coords"] = vector3(26.5906, -1392.0261, 27.3634),
+    },
+    [2] = {
+        ["label"] = "Hands Free Carwash",
+        ["coords"] = vector3(167.1034, -1719.4704, 27.2916),
+    },
+    [3] = {
+        ["label"] = "Hands Free Carwash",
+        ["coords"] = vector3(-74.5693, 6427.8715, 29.4400),
+    },
+    [4] = {
+        ["label"] = "Hands Free Carwash",
+        ["coords"] = vector3(-1200.4, -1720.46, 3.40),
+    },
+    [5] = {
+        ["label"] = "Hands Free Carwash",
+        ["coords"] = vector3(1363.22, 3592.7, 34.41),
+    },
+    [6] = {
+        ["label"] = "Hands Free Carwash",
+        ["coords"] = vector3(-699.6325, -932.7043, 17.0139),
+    }
+}
