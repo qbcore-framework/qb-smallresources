fade34f35f3ac755cb1dd866c27755a4320015de
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-06 16:12:40 +0200

    Update density.lua

--- client/density.lua
+++ client/density.lua
@@ -1,9 +1,8 @@
-Citizen.CreateThread(function()
+CreateThread(function()
 	while true do
 		SetVehicleDensityMultiplierThisFrame(0.05)
 	    SetPedDensityMultiplierThisFrame(1.0)
 	    SetParkedVehicleDensityMultiplierThisFrame(1.0)
-
-		Citizen.Wait(3)
+		Wait(3)
 	end
 end)
