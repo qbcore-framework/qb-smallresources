e7f6f90a51527035c503f7887f9d89b0c2675571
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2021-11-25 11:23:44 +0200

    add variables

--- client/density.lua
+++ client/density.lua
@@ -1,11 +1,18 @@
-local density = 0.8
+local Density = {
+    ['Parked'] = 0.8,
+    ['Vehicle'] = 0.8,
+    ['Multiplier'] = 0.8,
+    ['Peds'] = 0.8,
+    ['Scenario'] = 0.8,
+}
+
 CreateThread(function()
 	while true do
-		SetParkedVehicleDensityMultiplierThisFrame(density)
-		SetVehicleDensityMultiplierThisFrame(density)
-		SetRandomVehicleDensityMultiplierThisFrame(density)
-		SetPedDensityMultiplierThisFrame(density)
-		SetScenarioPedDensityMultiplierThisFrame(density, density) -- Walking NPC Density
+		SetParkedVehicleDensityMultiplierThisFrame(Density['Parked'])
+		SetVehicleDensityMultiplierThisFrame(Density['Vehicle'])
+		SetRandomVehicleDensityMultiplierThisFrame(Density['Multiplier'])
+		SetPedDensityMultiplierThisFrame(Density['Peds'])
+		SetScenarioPedDensityMultiplierThisFrame(Density['Scenario'], Density['Scenario']) -- Walking NPC Density
 		Wait(0)
 	end
 end)
