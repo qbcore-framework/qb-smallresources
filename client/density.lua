2ef8ac692ec5d78265599d0597c859d7f1187fee
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2021-12-07 09:23:58 +0200

    More choices density

--- client/density.lua
+++ client/density.lua
@@ -1,11 +1,34 @@
-local density = 0.8
+local density = {
+    ['parked'] = 0.8,
+    ['vehicle'] = 0.8,
+    ['multiplier'] = 0.8,
+    ['peds'] = 0.8,
+    ['scenario'] = 0.8,
+}
+
 CreateThread(function()
 	while true do
-		SetParkedVehicleDensityMultiplierThisFrame(density)
-		SetVehicleDensityMultiplierThisFrame(density)
-		SetRandomVehicleDensityMultiplierThisFrame(density)
-		SetPedDensityMultiplierThisFrame(density)
-		SetScenarioPedDensityMultiplierThisFrame(density, density) -- Walking NPC Density
+		SetParkedVehicleDensityMultiplierThisFrame(density['parked'])
+		SetVehicleDensityMultiplierThisFrame(density['vehicle'])
+		SetRandomVehicleDensityMultiplierThisFrame(density['multiplier'])
+		SetPedDensityMultiplierThisFrame(density['peds'])
+		SetScenarioPedDensityMultiplierThisFrame(density['scenario'], density['scenario']) -- Walking NPC Density
 		Wait(0)
 	end
 end)
+
+local function DecorSet(Type, Value)
+    if Type == 'parked' then
+        density['parked'] = Value
+    elseif Type == 'vehicle' then
+        density['vehicle'] = Value
+    elseif Type == 'multiplier' then
+        density['multiplier'] = Value
+    elseif Type == 'peds' then
+        density['peds'] = Value
+    elseif Type == 'scenario' then
+        density['scenario'] = Value
+    end
+end
+
+exports('DecorSet', DecorSet)
