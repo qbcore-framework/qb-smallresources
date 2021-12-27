c8e01abfe89a504c2eb5ac9f321c0b5592d15d64
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2021-12-27 09:37:36 +0200

    gang hostile

--- client/calmai.lua
+++ client/calmai.lua
@@ -1,14 +1,36 @@
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_HILLBILLY`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_FAMILY`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MARABUNTE`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_SALVA`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `AMBIENT_GANG_LOST`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_1`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_2`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_9`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `GANG_10`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `FIREMAN`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `MEDIC`, `PLAYER`)
-SetRelationshipBetweenGroups(1, `COP`, `PLAYER`)
+local relationshipTypes = {
+    "GANG_1",
+    "GANG_2",
+    "GANG_9",
+    "GANG_10",
+    "AMBIENT_GANG_LOST",
+    "AMBIENT_GANG_MEXICAN",
+    "AMBIENT_GANG_FAMILY",
+    "AMBIENT_GANG_BALLAS",
+    "AMBIENT_GANG_MARABUNTE",
+    "AMBIENT_GANG_CULT",
+    "AMBIENT_GANG_SALVA",
+    "AMBIENT_GANG_WEICHENG",
+    "AMBIENT_GANG_HILLBILLY",
+    "DEALER",
+    "HATES_PLAYER",
+    "NO_RELATIONSHIP",
+    "SPECIAL",
+    "MISSION2",
+    "MISSION3",
+    "MISSION4",
+    "MISSION5",
+    "MISSION6",
+    "MISSION7",
+    "MISSION8"
+}
+
+CreateThread(function()
+	while true do
+		Wait(50)
+		for _, group in ipairs(relationshipTypes) do
+			SetRelationshipBetweenGroups(3, GetHashKey('PLAYER'), GetHashKey(group)) -- 5 is always hostile
+			SetRelationshipBetweenGroups(3, GetHashKey(group), GetHashKey('PLAYER')) -- 5 is always hostile
+		end
+	end
+end)
