17f2cddf5922a627b28d1c7ed2622bcd239a4d5c
Atikur Rahaman Santo <55808925+ARSSANTO@users.noreply.github.com>
2022-01-02 21:13:04 +0600

    harness error fix while 1st time use

--- server/main.lua
+++ server/main.lua
@@ -22,7 +22,7 @@ end)
 RegisterNetEvent('equip:harness', function(item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    if Player.PlayerData.items[item.slot].info.uses - 1 == 0 then
+    if Player.PlayerData.items[item.slot].info.uses == 1 then
         TriggerClientEvent("inventory:client:ItemBox", src, QBCore.Shared.Items['harness'], "remove")
         Player.Functions.RemoveItem('harness', 1)
     else
