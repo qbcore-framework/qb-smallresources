113872b76d102952c8b66f88d045d33b0342c5ac
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-01-05 04:04:35 -0600

    corrected mapname.lua name
    updated and removed citizen. and updated name

--- client/mapname.lua
+++ client/mapname.lua
@@ -6,6 +6,6 @@ function AddTextEntry(key, value)
 	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
 end
 
-Citizen.CreateThread(function()
+CreateThread(function()
   AddTextEntry('FE_THDR_GTAO', '~y~ The Name | ~b~ OF Your Server, ~s~ RP..')
 end)
