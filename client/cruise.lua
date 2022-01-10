16214fc45657d952d1e491c1a2a49afa1b03a7c1
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-10 10:25:57 +0200

    clean up and GetCoreObject

--- client/cruise.lua
+++ client/cruise.lua
@@ -1,28 +1,8 @@
+local QBCore = exports['qb-core']:GetCoreObject()
 local Player = nil
-local CruisedSpeed, CruisedSpeedKm, VehicleVectorY = 0, 0, 0
+local CruisedSpeed, CruisedSpeedKm = 0, 0
 local vehicleClasses = {
-    [0] = true,
-    [1] = true,
-    [2] = true,
-    [3] = true,
-    [4] = true,
-    [5] = true,
-    [6] = true,
-    [7] = true,
-    [8] = true,
-    [9] = true,
-    [10] = true,
-    [11] = true,
-    [12] = true,
-    [13] = false,
-    [14] = false,
-    [15] = false,
-    [16] = false,
-    [17] = true,
-    [18] = true,
-    [19] = true,
-    [20] = true,
-    [21] = false
+    [0] = true, [1] = true, [2] = true, [3] = true, [4] = true, [5] = true, [6] = true, [7] = true, [8] = true, [9] = true, [10] = true, [11] = true, [12] = true, [13] = false, [14] = false, [15] = false, [16] = false, [17] = true, [18] = true, [19] = true, [20] = true, [21] = false
 }
 
 local function IsTurningOrHandBraking() return IsControlPressed(2, 76) or IsControlPressed(2, 63) or IsControlPressed(2, 64) end
