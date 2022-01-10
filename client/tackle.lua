afaffa4d100cdc3ebb16dcb3c9222d2dc4f23849
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-10 10:51:20 +0200

    add local object and function remove old import

--- client/tackle.lua
+++ client/tackle.lua
@@ -1,37 +1,6 @@
-CreateThread(function()
-    while true do 
-        if QBCore ~= nil then
-            local ped = PlayerPedId()
-            if not IsPedInAnyVehicle(ped, false) and GetEntitySpeed(ped) > 2.5 then
-                if IsControlJustPressed(1, 19) then
-                    Tackle()
-                end
-            else
-                Wait(250)
-            end
-        end
+local QBCore = exports['qb-core']:GetCoreObject()
 
-        Wait(1)
-    end
-end)
-
-RegisterNetEvent('tackle:client:GetTackled', function()
-	SetPedToRagdoll(PlayerPedId(), math.random(1000, 6000), math.random(1000, 6000), 0, 0, 0, 0) 
-	TimerEnabled = true
-	Wait(1500)
-	TimerEnabled = false
-end)
-
-function Tackle()
-    closestPlayer, distance = QBCore.Functions.GetClosestPlayer()
-    local closestPlayerPed = GetPlayerPed(closestPlayer)
-    if(distance ~= -1 and distance < 2) then
-        TriggerServerEvent("tackle:server:TacklePlayer", GetPlayerServerId(closestPlayer))
-        TackleAnim()
-    end
-end
-
-function TackleAnim()
+local function TackleAnim()
     local ped = PlayerPedId()
     if not QBCore.Functions.GetPlayerData().metadata["ishandcuffed"] and not IsPedRagdoll(ped) then
         RequestAnimDict("swimming@first_person@diving")
@@ -48,3 +17,35 @@ function TackleAnim()
         end
     end
 end
+
+local function Tackle()
+    closestPlayer, distance = QBCore.Functions.GetClosestPlayer()
+    local closestPlayerPed = GetPlayerPed(closestPlayer)
+    if(distance ~= -1 and distance < 2) then
+        TriggerServerEvent("tackle:server:TacklePlayer", GetPlayerServerId(closestPlayer))
+        TackleAnim()
+    end
+end
+
+RegisterNetEvent('tackle:client:GetTackled', function()
+	SetPedToRagdoll(PlayerPedId(), math.random(1000, 6000), math.random(1000, 6000), 0, 0, 0, 0) 
+	TimerEnabled = true
+	Wait(1500)
+	TimerEnabled = false
+end)
+
+CreateThread(function()
+    while true do 
+        if LocalPlayer.state.isLoggedIn then
+            local ped = PlayerPedId()
+            if not IsPedInAnyVehicle(ped, false) and GetEntitySpeed(ped) > 2.5 then
+                if IsControlJustPressed(1, 19) then
+                    Tackle()
+                end
+            else
+                Wait(250)
+            end
+        end
+        Wait(1)
+    end
+end)
