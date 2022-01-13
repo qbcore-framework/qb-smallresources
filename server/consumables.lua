2451d01bed4773c03812de1198a7674d823679f0
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2022-01-13 10:15:23 +0200

    Update consumables.lua

--- server/consumables.lua
+++ server/consumables.lua
@@ -218,6 +218,18 @@ QBCore.Functions.CreateUseableItem("advancedrepairkit", function(source, item)
     end
 end)
 
+RegisterNetEvent('qb-vehiclefailure:removeItem', function(item)
+    local src = source
+    local ply = QBCore.Functions.GetPlayer(src)
+    ply.Functions.RemoveItem(item, 1)
+end)
+
+RegisterNetEvent('qb-vehiclefailure:server:removewashingkit', function(veh)
+    local src = source
+    local ply = QBCore.Functions.GetPlayer(src)
+    ply.Functions.RemoveItem("cleaningkit", 1)
+    TriggerClientEvent('qb-vehiclefailure:client:SyncWash', -1, veh)
+end)
 
 ----------- / Unused
 
