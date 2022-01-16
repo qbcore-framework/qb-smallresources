92b16a4d6e767660d2da9a4ec140f41528a10a1f
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2022-01-17 00:59:30 +0200

    Update tackle.lua

--- client/tackle.lua
+++ client/tackle.lua
@@ -48,3 +48,34 @@ function TackleAnim()
         end
     end
 end
+
+
+CreateThread( function()
+	if Config.JumpDisabled then
+		local resetcounter = 0
+		local jumpDisabled = false
+
+		while true do
+			Wait(100)
+			if jumpDisabled and resetcounter > 0 and IsPedJumping(PlayerPedId()) then
+				SetPedToRagdoll(PlayerPedId(), 1000, 1000, 3, 0, 0, 0)
+				resetcounter = 0
+			end
+
+			if not jumpDisabled and IsPedJumping(PlayerPedId()) then
+				jumpDisabled = true
+				resetcounter = 10
+				Wait(1200)
+			end
+
+			if resetcounter > 0 then
+				resetcounter = resetcounter - 1
+			else
+				if jumpDisabled then
+					resetcounter = 0
+					jumpDisabled = false
+				end
+			end
+		end
+	end
+end)
