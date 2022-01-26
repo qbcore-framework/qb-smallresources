94f752fa0b0dcadefd0d9bd841c03a2e55d3c328
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2022-01-27 00:06:51 +0200

    New Density Adjustment

--- client/density.lua
+++ client/density.lua
@@ -1,34 +1,100 @@
-local density = {
-    ['parked'] = 0.8,
-    ['vehicle'] = 0.8,
-    ['multiplier'] = 0.8,
-    ['peds'] = 0.8,
-    ['scenario'] = 0.8,
-}
+DecorRegister('ScriptedPed', 2)
+local density = 0.8
+local MarkedPeds = {}
+local RequiredChecks = 4
+
+local function IsModelValid(ped)
+	local eType = GetPedType(ped)
+	return eType ~= 0 and eType ~= 1 and eType ~= 3 and eType ~= 28 and not IsPedAPlayer(ped)
+end
+
+local function IsPedValid(ped)
+	local isScripted = DecorExistOn(ped, 'ScriptedPed')
+	return not isScripted and IsModelValid(ped) and not IsEntityAMissionEntity(ped) and NetworkGetEntityIsNetworked(ped) and not IsPedDeadOrDying(ped, 1)  and IsPedStill(ped) and IsEntityStatic(ped) and not IsPedInAnyVehicle(ped) and not IsPedUsingAnyScenario(ped)
+end
+
+local function DeleteRoguePed(pPed)
+	local owner = NetworkGetEntityOwner(pPed)
+	if owner == -1 or owner == PlayerId() then
+		DeleteEntity(pPed)
+	else
+		local netId = NetworkGetNetworkIdFromEntity(pPed)
+		return { netId = netId, owner = GetPlayerServerId(owner)}
+	end
+end
+
+RegisterNetEvent('density:client:delete', function(pNetId)
+	local entity = NetworkGetEntityFromNetworkId(pNetId)
+	if DoesEntityExist(entity) then
+		DeleteEntity(entity)
+	end
+end)
+
+RegisterNetEvent('density:client:decorset', function(pNetId, pType, pProperty, pValue)
+	local entity = NetworkGetEntityFromNetworkId(pNetId)
+	if DoesEntityExist(entity) then
+		if pType == 1 then
+			DecorSetFloat(entity, pProperty, pValue)
+		elseif pType == 2 then
+			DecorSetBool(entity, pProperty, pValue)
+		elseif pType == 3 then
+			DecorSetInt(entity, pProperty, pValue)
+		end
+	end
+end)
 
 CreateThread(function()
 	while true do
-		SetParkedVehicleDensityMultiplierThisFrame(density['parked'])
-		SetVehicleDensityMultiplierThisFrame(density['vehicle'])
-		SetRandomVehicleDensityMultiplierThisFrame(density['multiplier'])
-		SetPedDensityMultiplierThisFrame(density['peds'])
-		SetScenarioPedDensityMultiplierThisFrame(density['scenario'], density['scenario']) -- Walking NPC Density
+		SetParkedVehicleDensityMultiplierThisFrame(density)
+		SetVehicleDensityMultiplierThisFrame(density)
+		SetRandomVehicleDensityMultiplierThisFrame(density)
+		SetPedDensityMultiplierThisFrame(density)
+		SetScenarioPedDensityMultiplierThisFrame(density, density)
 		Wait(0)
 	end
 end)
 
-function DecorSet(Type, Value)
-    if Type == 'parked' then
-        density['parked'] = Value
-    elseif Type == 'vehicle' then
-        density['vehicle'] = Value
-    elseif Type == 'multiplier' then
-        density['multiplier'] = Value
-    elseif Type == 'peds' then
-        density['peds'] = Value
-    elseif Type == 'scenario' then
-        density['scenario'] = Value
+CreateThread(function()
+    while true do
+		local idle = 2000
+		local success
+		local handle, ped = FindFirstPed()
+		repeat
+		
+		if IsPedValid(ped) and not MarkedPeds[ped] then
+			MarkedPeds[ped] = 1
+		end
+		
+		success, ped = FindNextPed(handle)
+		until not success
+		EndFindPed(handle)
+		Wait(idle)
     end
-end
+end)
 
-exports('DecorSet', DecorSet)
+CreateThread(function()
+    while true do
+		local idle = 3000
+		local toDelete = {}
+		local playerCoords = GetEntityCoords(PlayerPedId())
+
+	for ped, count in pairs(MarkedPeds) do
+		if ped and DoesEntityExist(ped) and IsPedValid(ped) then
+			local entitycoords = GetEntityCoords(ped)
+			if count >= RequiredChecks and #(entitycoords - playerCoords) <= 100.0 then
+			local deleteInfo = DeleteRoguePed(ped)
+				if deleteInfo then
+					toDelete[#toDelete+1] = deleteInfo
+				end
+			end
+			MarkedPeds[ped] = count + 1
+        else
+			MarkedPeds[ped] = nil
+        end
+	end
+	if #toDelete > 0 then
+        TriggerServerEvent('density:server:rogue', toDelete)
+	end
+		Wait(idle)
+    end
+end)
