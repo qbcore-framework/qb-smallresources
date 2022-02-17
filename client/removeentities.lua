d362fe7f52fb6ae43b7b5ffdeec06c8f4d943b19
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-02-17 18:16:58 +0200

    add more model

--- client/removeentities.lua
+++ client/removeentities.lua
@@ -1,18 +1,34 @@
-local objects = {
-    {x = 266.09, y = -349.35, z = 44.74, model = "prop_sec_barier_02b"}, 
-    {x = 285.28, y = -355.78, z = 45.13, model = "prop_sec_barier_02a"}, 
+local gates = {
+	'p_barier_test_s',
+	'prop_sec_barier_01a',
+	'prop_sec_barier_02a',
+	'prop_sec_barier_02b',
+	'prop_sec_barier_03a',
+	'prop_sec_barier_03b',
+	'prop_sec_barier_04a',
+	'prop_sec_barier_04b',
+	'prop_sec_barier_base_01',
+	'prop_sec_barrier_ld_01a',
+	'prop_sec_barrier_ld_02a',
+	'prop_gate_airport_01',
+	'prop_facgate_01',
+	'prop_facgate_03_l',
+	'prop_facgate_03_r',
+	'prop_gate_docks_ld'
 }
 
 CreateThread(function()
     while true do
-        for k, v in pairs(objects) do
-            local ent = GetClosestObjectOfType(objects[k].x, objects[k].y, objects[k].z, 2.0, GetHashKey(objects[k].model), false, false, false)
-
-            SetEntityAsMissionEntity(ent, 1, 1)
-            DeleteObject(ent)
-            SetEntityAsNoLongerNeeded(ent)
-        end
-
-        Wait(5000)
+		for i=1, #gates do
+			Wait(1)
+			local coords = GetEntityCoords(PlayerPedId(), false)
+			local gate = GetClosestObjectOfType(coords.x, coords.y, coords.z, 100.0, GetHashKey(gates[i]), 0, 0, 0)
+			if gate ~= 0 then
+				SetEntityAsMissionEntity(gate, 1, 1)
+				DeleteObject(gate)
+				SetEntityAsNoLongerNeeded(gate)
+			end
+		end
+	   Wait(10000)
     end
 end)
