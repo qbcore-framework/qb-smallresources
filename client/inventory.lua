3ddccd1b8e7a21e45678be817c86ea5617f4fd18
Restray <contact@restray.org>
2022-03-29 23:09:45 +0200

    Move snowballs events from qb-inventory

---
+++ client/inventory.lua
@@ -0,0 +1,25 @@
+RegisterNetEvent('smallresources:client:PickupSnowballs', function()
+    local ped = PlayerPedId()
+    LoadAnimDict('anim@mp_snowball')
+    TaskPlayAnim(ped, 'anim@mp_snowball', 'pickup_snowball', 3.0, 3.0, -1, 0, 1, 0, 0, 0)
+    QBCore.Functions.Progressbar("pickupsnowball", "Collecting snowballs..", 1500, false, true, {
+        disableMovement = true,
+        disableCarMovement = true,
+        disableMouse = false,
+        disableCombat = true,
+    }, {}, {}, {}, function() -- Done
+        ClearPedTasks(ped)
+        TriggerServerEvent('QBCore:Server:AddItem', "snowball", 1)
+        TriggerEvent('inventory:client:ItemBox', QBCore.Shared.Items["snowball"], "add")
+    end, function() -- Cancel
+        ClearPedTasks(ped)
+        QBCore.Functions.Notify("Canceled", "error")
+    end)
+end)
+
+RegisterNetEvent('smallresources:client:UseSnowball', function(amount)
+    local ped = PlayerPedId()
+    GiveWeaponToPed(ped, `weapon_snowball`, amount, false, false)
+    SetPedAmmo(ped, `weapon_snowball`, amount)
+    SetCurrentPedWeapon(ped, `weapon_snowball`, true)
+end)
