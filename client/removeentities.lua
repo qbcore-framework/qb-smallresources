cc9766aaa9bd8a85aaecae739ce2ddbaea1a518e
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-05-18 20:24:14 +0200

    Update client/removeentities.lua
    Co-authored-by: BerkieBb <82737367+BerkieBb@users.noreply.github.com>

--- client/removeentities.lua
+++ client/removeentities.lua
@@ -5,7 +5,7 @@ local objects = {
 
 CreateThread(function()
     while true do
-        for k, _ in pairs(objects) do
+        for k in pairs(objects) do
             local ent = GetClosestObjectOfType(objects[k].x, objects[k].y, objects[k].z, 2.0, GetHashKey(objects[k].model), false, false, false)
 
             SetEntityAsMissionEntity(ent, 1, 1)
