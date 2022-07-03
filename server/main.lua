ed62e93af997652b2580384bf4ba5119a27e34d2
erikmeyer08 <43043270+erikmeyer08@users.noreply.github.com>
2022-07-03 11:59:26 -0400

    Fixes Harness Inventory Metadata
    The metadata "uses" is not set when a harness is placed in a players inventory.
    This checks if the metadata "uses" is nil and if true then it sets uses to 19.
    This fixes the harnesses so they only have 20 uses and also fixes the server side exception as shown below.
    "[script:qb-smallresou] SCRIPT ERROR: @qb-smallresources/server/main.lua:39: attempt to perform arithmetic on a nil value (field 'uses')"

--- server/main.lua
+++ server/main.lua
@@ -20,13 +20,18 @@ end)
 RegisterNetEvent('equip:harness', function(item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    if Player.PlayerData.items[item.slot].info.uses == 1 then
+    
+    if Player.PlayerData.items[item.slot].info.uses == nil then
+        Player.PlayerData.items[item.slot].info.uses = 19
+        Player.Functions.SetInventory(Player.PlayerData.items)
+    elseif Player.PlayerData.items[item.slot].info.uses == 1 then
         TriggerClientEvent("inventory:client:ItemBox", src, QBCore.Shared.Items['harness'], "remove")
         Player.Functions.RemoveItem('harness', 1)
     else
         Player.PlayerData.items[item.slot].info.uses = Player.PlayerData.items[item.slot].info.uses - 1
         Player.Functions.SetInventory(Player.PlayerData.items)
     end
+    print(Player.PlayerData.items[item.slot].info.uses)
 end)
 
 RegisterNetEvent('seatbelt:DoHarnessDamage', function(hp, data)
