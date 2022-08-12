82e2e3e15741c8a87ef91379bf93b9db0250d6be
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2022-08-12 09:09:28 -0500

    fix(hudcomponents): don't wait four frames
    * The HideHudComponentThisFrame native needs to be called every frame waiting even just one frame (Wait(1)) causes a blinking issue (https://i.imgur.com/KGXkhOX.mp4) which can be seen in the bottom right of the screen.
    
    * Cleaned up code: component values are inside a table and will now be
      looped through, switched to using HudWeaponWheelIgnoreSelection()
      instead of calling 19 and 20 (component ids)

--- client/hudcomponents.lua
+++ client/hudcomponents.lua
@@ -1,30 +1,19 @@
-CreateThread(function()-- https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
-    while true do
-		HideHudComponentThisFrame(1) -- 1 : WANTED_STARS
-		HideHudComponentThisFrame(2) -- 2 : WEAPON_ICON
-		HideHudComponentThisFrame(3) -- 3 : CASH
-		HideHudComponentThisFrame(4) -- 4 : MP_CASH
-		-- HideHudComponentThisFrame(5)			-- 5 : MP_MESSAGE
-		-- HideHudComponentThisFrame(6)			-- 6 : VEHICLE_NAME
-		HideHudComponentThisFrame(7) -- 7 : AREA_NAME
-		-- HideHudComponentThisFrame(8)			-- 8 : VEHICLE_CLASS
-		HideHudComponentThisFrame(9) -- 9 : STREET_NAME
-		-- HideHudComponentThisFrame(10)		-- 10 : HELP_TEXT
-		-- HideHudComponentThisFrame(11)		-- 11 : FLOATING_HELP_TEXT_1
-		-- HideHudComponentThisFrame(12)		-- 12 : FLOATING_HELP_TEXT_2
-		HideHudComponentThisFrame(13) -- 13 : CASH_CHANGE
-		HideHudComponentThisFrame(14) -- 14 : RETICLE
-		-- HideHudComponentThisFrame(15)		-- 15 : SUBTITLE_TEXT
-		-- HideHudComponentThisFrame(16)		-- 16 : RADIO_STATIONS
-		HideHudComponentThisFrame(17) -- 17 : SAVING_GAME
-		-- HideHudComponentThisFrame(18)		-- 18 : GAME_STREAM
-		HideHudComponentThisFrame(19) -- 19 : WEAPON_WHEEL
-		HideHudComponentThisFrame(20) -- 20 : WEAPON_WHEEL_STATS
-		HideHudComponentThisFrame(21) -- 21 : HUD_COMPONENTS
-		HideHudComponentThisFrame(22) -- 22 : HUD_WEAPONS
-		DisableControlAction(1, 37)
-		DisplayAmmoThisFrame(true)
+-- 1 : WANTED_STARS | 4 : MP_CASH | 13 : CASH_CHANGE | 21 : HUD_COMPONENTS
+-- 2 : WEAPON_ICON | 7 : AREA_NAME | 14 : RETICLE | 22 : HUD_WEAPONS
+-- 3 : CASH | 9 : STREET_NAME | 17 : SAVING_GAME
 
-		Wait(4)
+local DISABLED_COMPONENTS <const> = {1, 2, 3, 4, 7, 9, 13, 14, 17, 21, 22}
+CreateThread(function()
+    while true do
+      for i = 1, #DISABLED_COMPONENTS do
+        HideHudComponentThisFrame(DISABLED_COMPONENTS[i])
+      end
+
+      -- Disable weapon wheel input
+      HudWeaponWheelIgnoreSelection()
+      DisableControlAction(1, 37)
+      DisplayAmmoThisFrame(true)
+
+      Wait(0)
     end
 end)
