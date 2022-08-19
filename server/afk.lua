739f7104aceed8d633a925ecdf5c27015569ae9b
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-08-19 15:38:46 +0200

    tweak(server/afk): optimize

--- server/afk.lua
+++ server/afk.lua
@@ -1,12 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('KickForAFK', function()
-    local src = source
-	DropPlayer(src, 'You Have Been Kicked For Being AFK')
+	DropPlayer(source, 'You Have Been Kicked For Being AFK')
 end)
 
 QBCore.Functions.CreateCallback('qb-afkkick:server:GetPermissions', function(source, cb)
-    local src = source
-    local group = QBCore.Functions.GetPermission(src)
-    cb(group)
+    cb(QBCore.Functions.GetPermission(source))
 end)
