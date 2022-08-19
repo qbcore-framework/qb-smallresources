8707322633107272984ac1731dc3daeb2570f2eb
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-08-19 15:26:24 +0200

    feat(client/calmai): prisoner

--- client/calmai.lua
+++ client/calmai.lua
@@ -1,3 +1,11 @@
+--  Relationship Types:
+--  0 = Companion
+--  1 = Respect
+--  2 = Like
+--  3 = Neutral
+--  4 = Dislike
+--  5 = Hate
+
 SetRelationshipBetweenGroups(1, `AMBIENT_GANG_HILLBILLY`, `PLAYER`)
 SetRelationshipBetweenGroups(1, `AMBIENT_GANG_BALLAS`, `PLAYER`)
 SetRelationshipBetweenGroups(1, `AMBIENT_GANG_MEXICAN`, `PLAYER`)
@@ -12,3 +20,4 @@ SetRelationshipBetweenGroups(1, `GANG_10`, `PLAYER`)
 SetRelationshipBetweenGroups(1, `FIREMAN`, `PLAYER`)
 SetRelationshipBetweenGroups(1, `MEDIC`, `PLAYER`)
 SetRelationshipBetweenGroups(1, `COP`, `PLAYER`)
+SetRelationshipBetweenGroups(1, `PRISONER`, `PLAYER`)
