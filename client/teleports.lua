b476368e1571fe077ada1de08da147c3935176a5
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-08-19 15:37:20 +0200

    tweak(client/teleports): optimize

--- client/teleports.lua
+++ client/teleports.lua
@@ -1,21 +1,37 @@
+local function DrawText3Ds(x, y, z, text)
+	SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(true)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x,y,z, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
+    ClearDrawOrigin()
+end
+
 CreateThread(function()
+    local sleep
     while true do
-        local inRange = false
+        sleep = 1000
         local ped = PlayerPedId()
         local pos = GetEntityCoords(ped)
 
-        for loc,_ in pairs(Config.Teleports) do
+        for loc in pairs(Config.Teleports) do
             for k, v in pairs(Config.Teleports[loc]) do
                 local dist = #(pos - vector3(v.coords.x, v.coords.y, v.coords.z))
                 if dist < 2 then
-                    inRange = true
+                    sleep = 0
                     DrawMarker(2, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.15, 255, 255, 255, 255, 0, 0, 0, 1, 0, 0, 0)
 
                     if dist < 1 then
                         DrawText3Ds(v.coords.x, v.coords.y, v.coords.z, v.drawText)
                         if IsControlJustReleased(0, 51) then
                             if k == 1 then
-                                if v["AllowVehicle"] then
+                                if v.AllowVehicle then
                                     SetPedCoordsKeepVehicle(ped, Config.Teleports[loc][2].coords.x, Config.Teleports[loc][2].coords.y, Config.Teleports[loc][2].coords.z)
                                 else
                                     SetEntityCoords(ped, Config.Teleports[loc][2].coords.x, Config.Teleports[loc][2].coords.y, Config.Teleports[loc][2].coords.z)
@@ -25,7 +41,7 @@ CreateThread(function()
                                     SetEntityHeading(ped, Config.Teleports[loc][2].coords.w)
                                 end
                             elseif k == 2 then
-                                if v["AllowVehicle"] then
+                                if v.AllowVehicle then
                                     SetPedCoordsKeepVehicle(ped, Config.Teleports[loc][1].coords.x, Config.Teleports[loc][1].coords.y, Config.Teleports[loc][1].coords.z)
                                 else
                                     SetEntityCoords(ped, Config.Teleports[loc][1].coords.x, Config.Teleports[loc][1].coords.y, Config.Teleports[loc][1].coords.z)
@@ -41,10 +57,6 @@ CreateThread(function()
             end
         end
 
-        if not inRange then
-            Wait(1000)
-        end
-
-        Wait(3)
+        Wait(sleep)
     end
 end)
