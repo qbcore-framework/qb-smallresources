e2afba7b48ff296daf31e8f86ed09fe2b3b6a0a7
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-08-19 15:39:53 +0200

    fix(server/main): harness usage

--- server/main.lua
+++ server/main.lua
@@ -1,3 +1,4 @@
+local QBCore = exports['qb-core']:GetCoreObject()
 local VehicleNitrous = {}
 
 RegisterNetEvent('tackle:server:TacklePlayer', function(playerId)
@@ -9,8 +10,7 @@ QBCore.Functions.CreateCallback('nos:GetNosLoadedVehs', function(_, cb)
 end)
 
 QBCore.Commands.Add("id", "Check Your ID #", {}, false, function(source)
-    local src = source
-    TriggerClientEvent('QBCore:Notify', src,  "ID: "..src)
+    TriggerClientEvent('QBCore:Notify', source,  "ID: "..source)
 end)
 
 QBCore.Functions.CreateUseableItem("harness", function(source, item)
@@ -20,11 +20,17 @@ end)
 RegisterNetEvent('equip:harness', function(item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    if Player.PlayerData.items[item.slot].info.uses == 1 then
+
+    if not Player then return end
+
+    if not Player.PlayerData.items[item.slot].info.uses then
+        Player.PlayerData.items[item.slot].info.uses = 19
+        Player.Functions.SetInventory(Player.PlayerData.items)
+    elseif Player.PlayerData.items[item.slot].info.uses == 1 then
         TriggerClientEvent("inventory:client:ItemBox", src, QBCore.Shared.Items['harness'], "remove")
         Player.Functions.RemoveItem('harness', 1)
     else
-        Player.PlayerData.items[item.slot].info.uses = Player.PlayerData.items[item.slot].info.uses - 1
+        Player.PlayerData.items[item.slot].info.uses -= 1
         Player.Functions.SetInventory(Player.PlayerData.items)
     end
 end)
@@ -33,10 +39,12 @@ RegisterNetEvent('seatbelt:DoHarnessDamage', function(hp, data)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 
+    if not Player then return end
+
     if hp == 0 then
         Player.Functions.RemoveItem('harness', 1, data.slot)
     else
-        Player.PlayerData.items[data.slot].info.uses = Player.PlayerData.items[data.slot].info.uses - 1
+        Player.PlayerData.items[data.slot].info.uses -= 1
         Player.Functions.SetInventory(Player.PlayerData.items)
     end
 end)
@@ -44,6 +52,8 @@ end)
 RegisterNetEvent('qb-carwash:server:washCar', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
+
+    if not Player then return end
 
     if Player.Functions.RemoveMoney('cash', Config.DefaultPrice, "car-washed") then
         TriggerClientEvent('qb-carwash:client:washCar', src)
@@ -56,8 +66,9 @@ end)
 
 QBCore.Functions.CreateCallback('smallresources:server:GetCurrentPlayers', function(_, cb)
     local TotalPlayers = 0
-    for _ in pairs(QBCore.Functions.GetPlayers()) do
-        TotalPlayers = TotalPlayers + 1
+    local players = QBCore.Functions.GetPlayers()
+    for _ in pairs(players) do
+        TotalPlayers += 1
     end
     cb(TotalPlayers)
 end)
