d2637ed43ce75f0a0313b4df71bf595f1be7302e
tehgooch <8425611+tehgooch@users.noreply.github.com>
2022-08-26 00:21:54 -0700

    Add disableHudComponents to configuration file
    Added Config.disableHudComponents variable with descriptions of each component that can be disabled and how to enable/disable them.

--- config.lua
+++ config.lua
@@ -12,6 +12,33 @@ Config.DefaultPrice = 20 -- Default price for the carwash
 Config.DirtLevel = 0.1 -- Threshold for the dirt level to be counted as dirty
 Config.DisableAmbience = false -- Disabled distance sirens, distance car alarms, etc
 
+-- Uncomment lines to disable the specified HUD component.
+-- Comment lines to enable the specified HUD component.
+Config.disableHudComponents = {
+	1,	-- WANTED_STARS
+	2,	-- WEAPON_ICON
+	3,	-- CASH
+	4,	-- MP_CASH
+  --5,	-- MP_MESSAGE
+  --6,	-- VEHICLE_NAME
+	7,	-- AREA_NAME
+  --8,	-- VEHICLE_CLASS
+	9,	-- STREET_NAME
+  --10,	-- HELP_TEXT
+  --11,	-- FLOATING_HELP_TEXT_1
+  --12,	-- FLOATING_HELP_TEXT_2
+	13,	-- CASH_CHANGE
+    14,	-- RETICLE
+  --15,	-- SUBTITLE_TEXT
+  --16,	-- RADIO_STATIONS
+  --17,	-- SAVING_GAME
+  --18,	-- GAME_STREAM
+	19,	-- WEAPON_WHEEL
+	20,	-- WEAPON_WHEEL_STATS
+	21,	-- HUD_COMPONENTS
+	22	-- HUD_WEAPONS
+}
+
 ConsumablesEat = {
     ["sandwich"] = math.random(35, 54),
     ["tosti"] = math.random(40, 50),
