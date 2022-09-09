9d16949dd8055a0bf109430b8f674ef484d97325
r0adra93 <github.com@legendsclan.org>
2022-09-09 01:27:04 -0400

    Alcohol Reduces Stress
    Have been running this on a server since July. Let's make this happen!

--- client/consumables.lua
+++ client/consumables.lua
@@ -186,6 +186,7 @@ RegisterNetEvent('consumables:client:DrinkAlcohol', function(itemName)
         TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items[itemName], "remove")
         TriggerServerEvent("consumables:server:drinkAlcohol", itemName)
         TriggerServerEvent("consumables:server:addThirst", QBCore.Functions.GetPlayerData().metadata["thirst"] + ConsumablesAlcohol[itemName])
+        TriggerServerEvent('hud:server:RelieveStress', math.random(2, 4))
         alcoholCount += 1
         if alcoholCount > 1 and alcoholCount < 4 then
             TriggerEvent("evidence:client:SetStatus", "alcohol", 200)
