cb3387802c7ac02d11fec306ea9ac995f6b18c33
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2022-09-12 14:33:45 -0400

    add objects to config

--- config.lua
+++ config.lua
@@ -126,6 +126,11 @@ Config.BlacklistedPeds = {
     [`s_m_y_hwaycop_01`] = true,
 }
 
+Config.Objects = { -- for object removal
+    {coords = vector3(266.09,-349.35,44.74), heading = 0, length = 200, width = 200, model = "prop_sec_barier_02b"},
+    {coords = vector3(285.28,-355.78,45.13), heading = 0, length = 200, width = 200, model = "prop_sec_barier_02a"},
+}
+
 Config.Teleports = {
     --Elevator @ labs
     [1] = {
