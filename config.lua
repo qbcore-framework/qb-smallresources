ac091ce1c24176b50e0c8d55773695fe0442b8a2
Charlie <dev@charlie.fyi>
2022-09-23 13:40:22 +0100

    Fix merge error

--- config.lua
+++ config.lua
@@ -29,6 +29,7 @@ Config.DispatchServices = {
     ["PoliceVehicleRequest"] = false,
     ["SwatAutomobile"] = false,
     ["SwatHelicopter"] = false,
+}
 
 Config.Disable = {
     disableHudComponents = {1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
