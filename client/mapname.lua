aafde332e048d5340d2ed283a601112db01627ea
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-12-09 17:11:37 +0100

    Update client/mapname.lua

--- client/mapname.lua
+++ client/mapname.lua
@@ -1,7 +1,3 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
 CreateThread(function()
   Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), "FE_THDR_GTAO", Config.MapText)
 end)
