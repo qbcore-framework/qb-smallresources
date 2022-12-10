1aa302b424fcd0843d2dcdffb59a0f1cde523487
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-12-10 22:21:14 +0100

    Remove unneeded imports from polyzone

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,8 +13,6 @@ server_script 'server/*.lua'
 client_scripts {
     '@PolyZone/client.lua',
     '@PolyZone/BoxZone.lua',
-    '@PolyZone/EntityZone.lua',
-    '@PolyZone/CircleZone.lua',
     '@PolyZone/ComboZone.lua',
     'client/*.lua'
 }
