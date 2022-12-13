093d35a8cce6b94cc8dc7c6e06f526b5fc59e483
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2022-12-13 06:16:10 -0500

    Bug Fix
    for some reason if this keymapping is shared with any other key mapping at all it bugs when trying to drive a vehicle after canceling an animation.

--- client/handsup.lua
+++ client/handsup.lua
@@ -1,7 +1,6 @@
 local animDict = "missminuteman_1ig_2"
 local anim = "handsup_base"
 local handsup = false
-local disableHandsupControls = Config.disableHandsupControls
 
 RegisterCommand('hu', function()
     local ped = PlayerPedId()
@@ -15,13 +14,11 @@ RegisterCommand('hu', function()
     if exports['qb-policejob']:IsHandcuffed() then return end
     if handsup then
         TaskPlayAnim(ped, animDict, anim, 8.0, 8.0, -1, 50, 0, false, false, false)
-        exports['qb-smallresources']:addDisableControls(disableHandsupControls)
+        exports['qb-smallresources']:addDisableControls(Config.disableHandsupControls)
     else
         ClearPedTasks(ped)
-        exports['qb-smallresources']:removeDisableControls(disableHandsupControls)
+        exports['qb-smallresources']:removeDisableControls(Config.disableHandsupControls)
     end
 end, false)
-
-RegisterKeyMapping('hu', 'Put your hands up', 'KEYBOARD', 'X')
 
 exports('getHandsup', function() return handsup end)
