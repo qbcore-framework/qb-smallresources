b40afbe3b06836aca3dbe499a050c0f13fd737e6
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-12-17 23:22:07 +0100

    Make max players update dynamically

--- client/discord.lua
+++ client/discord.lua
@@ -11,7 +11,7 @@ CreateThread(function()
 
         if conf.ShowPlayerCount then
             QBCore.Functions.TriggerCallback('smallresources:server:GetCurrentPlayers', function(result)
-                SetRichPresence('Players: ' .. result..'/64')
+                SetRichPresence('Players: ' .. result .. '/' .. tostring(GetConvarInt('sv_maxclients', 64)))
             end)
         end
 
