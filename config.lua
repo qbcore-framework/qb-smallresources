c33a3325c0fb526289db9089a8544bb2f8f7a22e
Charlie <dev@charlie.fyi>
2022-12-17 22:31:14 +0000

    [ENHANCEMENT] Improve discord rich presence (#273)
    * Moving discord data to Config
    
    Co-authored-by: Zerio <54480523+Z3rio@users.noreply.github.com>

--- config.lua
+++ config.lua
@@ -35,6 +35,29 @@ Config.DispatchServices = {
 -- Enable or disable the wanted level
 Config.EnableWantedLevel = false
 
+-- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
+Config.Discord = {
+    ["IsEnabled"] = true, -- If set to true, then discord rich presence will be enabled
+    ["ApplicationId"] = '915674222628585472', -- The discord application id
+    ["IconLarge"] = 'logo_name', -- The name of the large icon
+    ["IconLargeHoverText"] = 'This is a Large icon with text', -- The hover text of the large icon
+    ["IconSmall"] = 'small_logo_name', -- The name of the small icon
+    ["IconSmallHoverText"] = 'This is a Small icon with text', -- The hover text of the small icon
+    ["UpdateRate"] = 60000, -- How often the player count should be updated
+    ["ShowPlayerCount"] = true, -- If set to true the player count will be displayed in the rich presence
+    ["MaxPlayers"] = 48, -- Maximum amount of players
+    ["Buttons"] = {
+        {
+            text = 'First Button!',
+            url = 'fivem://connect/localhost:30120'
+        },
+        {
+            text = 'Second Button!',
+            url = 'fivem://connect/localhost:30120'
+        }
+    }
+}
+
 Config.Disable = {
     disableHudComponents = {1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
     disableControls = {37}, -- Controls: https://docs.fivem.net/docs/game-references/controls/
