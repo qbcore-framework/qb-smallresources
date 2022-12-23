ed347d10172b81f9dbe915db90ed0880de975822
Tray <30093468+iktray@users.noreply.github.com>
2022-12-22 21:39:16 -0500

    Duplicate Config.

--- config.lua
+++ config.lua
@@ -66,12 +66,6 @@ Config.Discord = {
     }
 }
 
-Config.Disable = {
-    disableHudComponents = {1, 2, 3, 4, 7, 9, 13, 14, 19, 20, 21, 22}, -- Hud Components: https://docs.fivem.net/natives/?_0x6806C51AD12B83B8
-    disableControls = {37}, -- Controls: https://docs.fivem.net/docs/game-references/controls/
-    displayAmmo = true -- false disables ammo display
-}
-
 Config.Density = {
     ['parked'] = 0.8,
     ['vehicle'] = 0.8,
