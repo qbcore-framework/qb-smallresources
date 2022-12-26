f6267d115bdcbec24b735e56395298918981e407
Lior Koren <45363723+fsocietystudios@users.noreply.github.com>
2022-12-26 22:34:15 +0200

    Matched ``pt`` with ``en``
    Matched ``pt`` with ``en`` as ``pt`` on was way out of context. (It wasn't the right keys in the table + and missed some too)

--- locales/pt.lua
+++ locales/pt.lua
@@ -1,38 +1,68 @@
 local Translations = {
-    error = {
-        ["afk_time_left_minutes"] = "Estás AFK e vais ser kickado em %{timeLeft} minutos!",
-        ["afk_time_left_minute"] = "Estás AFK e vais ser kickado em %{timeLeft} minuto!",
-        ["afk_time_left_seconds"] = "Estás AFK e vais ser kickado em %{timeLeft} segundos!",
-        ["car_wash_canceled"] = "Lavagem cancelada...",
-        ["car_wash_notdirty"] = "O veículo não está sujo",
-        ["cruise_deactivated"] = "Cruise control desativado",
-        ["cruise_unavailable"] = "Cruise control indisponível",
-        ["not_in_car"] = "Não estás dentro de um carro.",
-        ["dont_have_enough_money"] = "Não tens dinheiro suficiente...",
-        ["global_canceled"] = "Cancelado...",
+    afk = {
+        afk_kick_message = 'Por estar AFK, você foi expulso do servidor.',
+        will_kick = 'Você está AFK e será chutado ',
+        time_seconds = ' segundos!',
+        time_minutes = ' minuto(s)!'
     },
-    info = {
-        ["cruise_activated_mp"] = "Cruise Ativado: %{speed} MP/H",
-        ["cruise_activated_km"] = "Cruise Ativado: %{speed} KM/H",
+    wash = {
+        in_progress = "O veículo está a ser lavado...",
+        wash_vehicle = "[E] Lavar Veículo",
+        wash_vehicle_target = "Lavar Veículo",
+        dont_have_enough_money = "Você não tem dinheiro suficiente para lavar seu veículo.",
+        dirty = "O veículo não está sujo",
+        cancel = "Lavagem cancelada...",
     },
-    progress = {
-        ["car_wash_progress"] = "O veículo está a ser lavado...",
-        ["placing_firework"] = "A preparar fogo de artifício...",
-        ["attach_race_harness"] = "A colocar Arnês de Corrida",
-        ["remove_race_harness"] = "A remover Arnês de Corrida",
+    consumables = {
+        eat_progress = "Comendo..",
+        drink_progress = "Bebendo..",
+        liqour_progress = "Beber licor..",
+        coke_progress = "Cheirar rápido..",
+        crack_progress = "Fumar crack..",
+        ecstasy_progress = "Pills Pops",
+        healing_progress = "Curando",
+        meth_progress = "Fumar metanfetamina",
+        joint_progress = "Junta de iluminação..",
+        use_parachute_progress = "Colocando paraquedas..",
+        pack_parachute_progress = "Pára-quedas de embalagem..",
+        no_parachute = "voce nao tem paraquedas!",
+        armor_full = "Você já tem armadura suficiente!",
+        armor_empty = "Você não está vestindo um colete..",
+        armor_progress = "Colocando a armadura corporal..",
+        heavy_armor_progress = "Colocando a armadura pesada..",
+        remove_armor_progress = "Removendo a armadura corporal..",
+        canceled = "Cancelado..",
     },
-    text = {
-        ["afk_kick_message"] = "Foste Kickado Por Estares AFK",
-        ["car_wash_text"] = "~g~E~w~ - Lavar Veículo (%{price}€)",
-        ["car_wash_not_available"] = "A estação de lavagem não está disponível...",
-        ["time_until_firework"] = "Fogo de Artifício em ~r~%{time}",
-        ["push_vehicle"] = "[~g~SHIFT~w~] + [~g~E~w~] para empurrar veículo",
+    cruise = {
+        unavailable = "Controle de cruzeiro indisponível",
+        activated = "Cruzeiro ativado: ",
+        deactivated = "Cruzeiro desativado",
     },
     editor = {
-        ["record"] = "Gravação Iniciada!",
-        ["save"] = "Gravação Guardada!",
-        ["delete_clip"] = "Gravação Apagada!",
-        ["editor"] = "Later aligator!",
+        started = "Gravação Iniciada!",
+        save = "Gravação salva!",
+        delete = "Gravação deletada!",
+        editor = "Mais tarde jacaré!"
+    },
+    firework = {
+        place_progress = "Colocar objeto..",
+        canceled = "Cancelado..",
+        time_left = "Fogo de artifício acabou ~r~"
+    },
+    seatbelt = {
+        use_harness_progress = "Prendendo o Arnês de Corrida",
+        remove_harness_progress = "Removendo o arnês de corrida",
+        no_car = "Você não está em um carro."
+    },
+    teleport = {
+        teleport_default = 'Usar elevador'
+    },
+    pushcar = {
+        stop_push = "[E] Pare de Empurrar"
     }
 }
-Lang = Locale:new({phrases = Translations, warnOnMissing = true})
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
