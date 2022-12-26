4d5e5fe77bcc3457b5aded0e4ce795a74aa92e1a
Friskky <brendanco7@hotmail.com>
2022-12-25 19:17:10 -0800

    Added Blacklisted vehicles with my qb-core PR. Added MidAirVehControl and AntiCombatRoll Configs. (Self Explanatory)

--- client/ignore.lua
+++ client/ignore.lua
@@ -101,3 +101,58 @@ CreateThread(function()
         SetPedDropsWeaponsWhenDead(v, false)
     end
 end)
+
+CreateThread(function()
+    while Config.AntiCombatRoll do
+        Wait(5)
+        if IsAimCamActive(bool) then
+            if IsPedArmed(GetPlayerPed(-1), 4 | 2) then 
+                DisableControlAction(0, 22, true)
+            end
+        end
+    end
+end)
+
+local vehicleClassDisableControl = {
+	[0] = true, --compacts
+	[1] = true, --sedans
+	[2] = true, --SUV's
+	[3] = true, --coupes
+	[4] = true, --muscle
+	[5] = true, --sport classic
+	[6] = true, --sport
+	[7] = true, --super
+	[8] = false, --motorcycle
+	[9] = true, --offroad
+	[10] = true, --industrial
+	[11] = true, --utility
+	[12] = true, --vans
+	[13] = false, --bicycles
+	[14] = false, --boats
+	[15] = false, --helicopter
+	[16] = false, --plane
+	[17] = true, --service
+	[18] = true, --emergency
+	[19] = false --military
+}
+	
+-- Main thread
+CreateThread(function()
+	while Config.MidAirVehControl do
+	-- Loop forever and update every frame
+	Wait(0)	
+		-- Get player, vehicle and vehicle class
+		local player = GetPlayerPed(-1)
+		local vehicle = GetVehiclePedIsIn(player, false)
+		local vehicleClass = GetVehicleClass(vehicle)
+
+		-- Disable control if player is in the driver seat and vehicle class matches array
+		if ((GetPedInVehicleSeat(vehicle, -1) == player) and vehicleClassDisableControl[vehicleClass]) then
+			-- Check if vehicle is in the air and disable L/R and UP/DN controls
+			if IsEntityInAir(vehicle) then
+				DisableControlAction(2, 59)
+				DisableControlAction(2, 60)
+			end
+		end
+	end
+end)
