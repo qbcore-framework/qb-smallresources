21b3cbdda5d294f6dd87bc838c0239a6b21b85d7
Bakisito <73045761+Bakisito@users.noreply.github.com>
2023-01-12 09:42:24 -0400

    Update handsup.lua

--- client/handsup.lua
+++ client/handsup.lua
@@ -1,7 +1,8 @@
+QBCore = exports["qb-core"]:GetCoreObject()
+
 local animDict = "missminuteman_1ig_2"
 local anim = "handsup_base"
 local handsup = false
-
 RegisterCommand('hu', function()
     local ped = PlayerPedId()
     if not HasAnimDictLoaded(animDict) then
@@ -10,15 +11,20 @@ RegisterCommand('hu', function()
             Wait(10)
         end
     end
-    handsup = not handsup
-    if exports['qb-policejob']:IsHandcuffed() then return end
-    if handsup then
-        TaskPlayAnim(ped, animDict, anim, 8.0, 8.0, -1, 50, 0, false, false, false)
-        exports['qb-smallresources']:addDisableControls(Config.disableHandsupControls)
-    else
-        ClearPedTasks(ped)
-        exports['qb-smallresources']:removeDisableControls(Config.disableHandsupControls)
-    end
+            -- We verify if the character is dead or in last stand or handcuffed.
+    QBCore.Functions.GetPlayerData(function(PlayerData)
+        if PlayerData.metadata["isdead"] or exports['qb-policejob']:IsHandcuffed() or PlayerData.metadata["inlaststand"] then
+            return
+        end
+        handsup = not handsup
+        if handsup then
+            TaskPlayAnim(ped, animDict, anim, 8.0, 8.0, -1, 50, 0, false, false, false)
+            exports['qb-smallresources']:addDisableControls(Config.disableHandsupControls)
+        else
+            ClearPedTasks(ped)
+            exports['qb-smallresources']:removeDisableControls(Config.disableHandsupControls)
+        end
+    end)
 end, false)
 
 exports('getHandsup', function() return handsup end)
