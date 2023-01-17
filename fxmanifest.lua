66b01c6a0ddccd9537eb18e24f8d7558bdada235
r0adra93 <github.com@legendsclan.org>
2023-01-16 21:40:29 -0500

    Update fxmanifest.lua
    I don' tknow how I removed those extras I put them back.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,21 @@ game 'gta5'
 description 'QB-SmallResources'
 version '1.3.0'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua', -- Change to the language you want
+    'config.lua'
+}
 server_scripts {
 	'configServer.lua',
 	'server/*.lua'
 }
-client_script 'client/*.lua'
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client/*.lua'
+}
 
 data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
 data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
