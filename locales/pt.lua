cffe830fd375ba7c936f19b7297735d2143c0f90
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2023-02-16 08:32:14 +0100

    Update pt.lua
    Added missing translations.  Corrected GetConvar to follow qbcore standards. Tested! Everything ok!

--- locales/pt.lua
+++ locales/pt.lua
@@ -1,38 +1,69 @@
 local Translations = {
-    error = {
-        ["afk_time_left_minutes"] = "Estás AFK e vais ser kickado em %{timeLeft} minutos!",
-        ["afk_time_left_minute"] = "Estás AFK e vais ser kickado em %{timeLeft} minuto!",
-        ["afk_time_left_seconds"] = "Estás AFK e vais ser kickado em %{timeLeft} segundos!",
-        ["car_wash_canceled"] = "Lavagem cancelada...",
-        ["car_wash_notdirty"] = "O veículo não está sujo",
-        ["cruise_deactivated"] = "Cruise control desativado",
-        ["cruise_unavailable"] = "Cruise control indisponível",
-        ["not_in_car"] = "Não estás dentro de um carro.",
-        ["dont_have_enough_money"] = "Não tens dinheiro suficiente...",
-        ["global_canceled"] = "Cancelado...",
+    afk = {
+        will_kick = 'Estás AFK e vais ser kickado em ',
+        time_seconds = ' segundo(s)!',
+        time_minutes = ' minuto(s)!'
     },
-    info = {
-        ["cruise_activated_mp"] = "Cruise Ativado: %{speed} MP/H",
-        ["cruise_activated_km"] = "Cruise Ativado: %{speed} KM/H",
+    wash = {
+        in_progress = "A lavar o veículo ..",
+        wash_vehicle = "[E] Lavar Veículo",
+        wash_vehicle_target = "Lavar Vehicle",
+        dirty = "O veículo não está sujo",
+        cancel = "Lavagem Cancelada ..",
     },
-    progress = {
-        ["car_wash_progress"] = "O veículo está a ser lavado...",
-        ["placing_firework"] = "A preparar fogo de artifício...",
-        ["attach_race_harness"] = "A colocar Arnês de Corrida",
-        ["remove_race_harness"] = "A remover Arnês de Corrida",
+    consumables = {
+        eat_progress = "A comer..",
+        drink_progress = "A beber..",
+        liqour_progress = "A beber alcool..",
+        coke_progress = "Cheirar uma linha..",
+        crack_progress = "A fumar crack..",
+        ecstasy_progress = "A ingerir pastilhas",
+        healing_progress = "A curar...",
+        meth_progress = "Smoking Ass Meth",
+        joint_progress = "A fumar metanfetamina...",
+        use_parachute_progress = "A colocar pára-quedas..",
+        pack_parachute_progress = "A preparar pára-quedas...",
+        no_parachute = "Não possui pára-quedas!",
+        armor_full = "Já tem armadura suficiente no seu colete",
+        armor_empty = "Não está a usar o colete...",
+        armor_progress = "A colocar armadura",
+        heavy_armor_progress = "A colocar armadura pesada..",
+        remove_armor_progress = "A removere armadura..",
+        canceled = "Cancelado..",
     },
-    text = {
-        ["afk_kick_message"] = "Foste Kickado Por Estares AFK",
-        ["car_wash_text"] = "~g~E~w~ - Lavar Veículo (%{price}€)",
-        ["car_wash_not_available"] = "A estação de lavagem não está disponível...",
-        ["time_until_firework"] = "Fogo de Artifício em ~r~%{time}",
-        ["push_vehicle"] = "[~g~SHIFT~w~] + [~g~E~w~] para empurrar veículo",
+    cruise = {
+        unavailable = "Cruise control não disponivel",
+        activated = "Cruise control activado: ",
+        deactivated = "Cruise control desactivado",
     },
     editor = {
-        ["record"] = "Gravação Iniciada!",
-        ["save"] = "Gravação Guardada!",
-        ["delete_clip"] = "Gravação Apagada!",
-        ["editor"] = "Later aligator!",
+        started = "A gravar!",
+        save = "Gravação guardada",
+        delete = "Gravação apagada!",
+        editor = "Até logo!"
+    },
+    firework = {
+        place_progress = "A colocar objecto..",
+        canceled = "Cancelado..",
+        time_left = "Fogo de artificio terminado ~r~"
+    },
+    seatbelt = {
+        use_harness_progress = "A colocar o arnês de corrida",
+        remove_harness_progress = "A remover o arnês de corrida",
+        no_car = "Não se encontra num veículo"
+    },
+    teleport = {
+        teleport_default = 'Usar Elevador'
+    },
+    pushcar = {
+        stop_push = "[E] Para de Empurrar"
     }
 }
-Lang = Locale:new({phrases = Translations, warnOnMissing = true})
+
+if GetConvar('qb_locale', 'en') == 'pt' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
