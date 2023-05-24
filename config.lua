ef2761ea40a8b343e67c6fc288d205f6f6a4ae0e
AnishBplayz <91739770+AnishBplayz@users.noreply.github.com>
2023-05-24 17:37:53 +0530

    Config option for Remove Pistol Whipping

--- config.lua
+++ config.lua
@@ -86,6 +86,8 @@ Config.Stun = {
     max = 7000
 }
 
+Config.RemovePistolWhipping = true
+
 Config.Cruise = 'mp/h'
 Config.IdleCamera = true
 Config.disableHandsupControls = {24, 25, 47, 58, 59, 63, 64, 71, 72, 75, 140, 141, 142, 143, 257, 263, 264}
