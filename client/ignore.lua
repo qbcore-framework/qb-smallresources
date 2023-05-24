8f3765ebda8c5d53e4169e43756dacb75deb138d
AnishBplayz <91739770+AnishBplayz@users.noreply.github.com>
2023-05-24 17:36:05 +0530

    Ignore.lua / Pistolwhipping

--- client/ignore.lua
+++ client/ignore.lua
@@ -112,3 +112,16 @@ CreateThread(function()
         end
     end
 end)
+
+CreateThread(function()
+    while (Config.RemovePistolWhipping) do
+        if IsPedAiming(PlayerPedId()) then
+            DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        else
+            Wait(200)
+        end
+        Wait(4)
+    end
+end)
