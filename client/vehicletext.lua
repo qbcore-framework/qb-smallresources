1f193411cf486243980d585c55ce439deedc2ceb
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2023-12-26 11:09:05 +0300

    Update vehicletext.lua

--- client/vehicletext.lua
+++ client/vehicletext.lua
@@ -1,15 +1,1 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-CreateThread(function()
-    for _, v in pairs(QBCore.Shared.Vehicles) do
-        local text
-        if v.brand then
-            text = v.brand..' '..v.name
-        else
-            text = v.name
-        end
-        if v.hash and v.hash ~= 0 then
-            AddTextEntryByHash(v.hash, text)
-        end
-    end
-end)
+-- move to vehicle.lua
