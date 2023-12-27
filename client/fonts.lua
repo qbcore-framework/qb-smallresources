f3a17ed3ff41aeddd82f015dcb6017bff845050d
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2023-12-27 09:46:57 +0300

    Create fonts.lua
    Creating a font file in the project, so that all fonts from all languages can be added will be of good use to some project managers

---
+++ client/fonts.lua
@@ -0,0 +1,32 @@
+function RegisterCustomFont(dict, name, lang)
+    if not Config.FontList[name] then
+        Config.FontList[name] = { dict = dict, name = name, lang = lang or 'en', id = nil }
+    end
+
+    if not Config.FontList[name].id then
+        RegisterFontFile(dict)
+        Config.FontList[name].id = RegisterFontId(name)
+    end
+
+    return Config.FontList[name].id
+end
+
+exports('RegisterCustomFont', RegisterCustomFont)
+
+function GetCustomFontId(name)
+    return Config.FontList[name] and Config.FontList[name].id or nil
+end
+
+exports('GetCustomFontId', GetCustomFontId)
+
+function GetAvailableFonts()
+    return Config.FontList
+end
+
+exports('GetAvailableFonts', GetAvailableFonts)
+
+CreateThread(function()
+    for _, font in pairs(Config.FontList) do
+        RegisterCustomFont(font.dict, font.name, font.lang)
+    end
+end
